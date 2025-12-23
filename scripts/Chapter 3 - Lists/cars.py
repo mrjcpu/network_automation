bc68581a44d8fa17613b10b7853722beb159f183
mrjcpu <morganj@mrjcpu.com>
2025-12-22 17:09:46 -0700

    Chapter 3 Completed

---
+++ scripts/Chapter 3 - Lists/cars.py
@@ -0,0 +1,16 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+len(cars)
+print(cars)
+
+#print("Here is the original list:")
+#print(cars)
+
+#print("\nHere is the sorted list:")
+#print(sorted(cars))
+
+#print("\nHere is the original list again:")
+#print(cars)
+
+#cars.reverse()
+#print(cars)
+
---
+++ scripts/Chapter 3 - Lists/seeing_the_world.py
@@ -0,0 +1,27 @@
+locations = ['hawaii', 'new york', 'brazil', 'japan', 'russia']
+print(locations)
+
+print(sorted(locations))
+
+print(locations)
+
+print(sorted(locations, reverse=True))
+
+print(locations)
+
+locations.reverse()
+print(locations)
+
+locations.reverse()
+print(locations)
+
+print(sorted(locations))
+
+print(sorted(locations, reverse=True))
+
+print(f"{len(locations)} places are on my bucket list for visiting.")
+
+print(locations[6])
+
+
+
